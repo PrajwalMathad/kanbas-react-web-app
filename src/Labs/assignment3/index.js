e86dc9cd08fd62db8e995a384fc4a625c5147f7b
PrajwalMathad <prajwalmathad966@gmail.com>
2023-10-14 17:16:35 -0400

    Labs for A3

---
+++ src/Labs/assignment3/index.js
@@ -0,0 +1,25 @@
+import JavaScript from "./JavaScript";
+import PathParameters from "./PathParameters";
+import TodoList from "./todo/TodoList";
+import DynamicStyling from "./DynamicStyling";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+
+function Assignment3() {
+    return (
+        <div>
+            <h2>Assignment 3</h2>
+            <TodoList />
+            <hr />
+            <ConditionalOutput />
+            <Classes />
+            <Styles />
+            <PathParameters />
+            <JavaScript />
+            <DynamicStyling />
+        </div>
+    );
+}
+
+export default Assignment3;
