d8fd4e2c924879bad5e0665880b856be326ebdea
PrajwalMathad <prajwalmathad966@gmail.com>
2023-10-19 15:31:49 -0400

    Navigation Adjustment

--- src/Kanbas/Courses/CourseNavigation/index.js
+++ src/Kanbas/Courses/CourseNavigation/index.js
@@ -2,9 +2,9 @@ import { Link, useParams, useLocation } from "react-router-dom";
 import "./index.css";
 
 function CourseNavigation() {
-  const links = ["Home", "Modules", "Piazza", "Zoom Meetings", "Assignments", "Quizzes", "Grades", "People",
-    "Panopto Video", "Discussions", "Announcements", "Pages", "Files", "Rubrics", "Outcomes", "Collaborations",
-    "Syllabus", "Progress Reports[EAB Navigate]", "Settings"];
+  const links = ["Home", "Modules", "Piazza", "Assignments", "Quizzes", "Grades", "People",
+     "Discussions", "Announcements", "Pages", "Files", "Rubrics", "Panopto Video", "Outcomes", "Collaborations",
+    "Syllabus", "Zoom Meetings", "Progress Reports[EAB Navigate]", "Settings"];
   const { courseId } = useParams();
   const { pathname } = useLocation();
   return (
